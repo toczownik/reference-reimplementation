175f5b52a2491231f521d81b74d401a976eef82f
Aleksander Mielczarek <olek@olek.pro>
2023-02-06 20:16:48 +0100

    added image display

---
+++ Image_displayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by codac-dev on 06.02.23.
+//
+
+#include "Image_displayer.h"
---
+++ Image_displayer.h
@@ -0,0 +1,14 @@
+//
+// Created by codac-dev on 06.02.23.
+//
+
+#ifndef REFERENCE_REIMPLEMENTATION_IMAGE_DISPLAYER_H
+#define REFERENCE_REIMPLEMENTATION_IMAGE_DISPLAYER_H
+
+
+class Image_displayer {
+
+};
+
+
+#endif //REFERENCE_REIMPLEMENTATION_IMAGE_DISPLAYER_H
